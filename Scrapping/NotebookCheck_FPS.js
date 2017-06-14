53bc836bba270bf85070e3b972f237e0628cdaf1
Zachary Dahan <hello@zachary.pm>
2017-06-14 03:09:07 +0200

    feat(scrap-fps): Introducing NotebookCheck Scrapping. 🛠

---
+++ Scrapping/NotebookCheck_FPS.js
@@ -0,0 +1,143 @@
+const Scrape = require('./class/Scrape');
+const sanitizer = require('./utils/SanitizerUtil');
+const mapLimit = require('async/mapLimit');
+const gpuAttribute = require('./types/gpuAttribute');
+
+class NotebookCheck_FPS extends Scrape {
+  constructor(url) {
+    super();
+
+    this.gameList = [];
+    this.qualityList = [];
+    this.gpuList = [];
+    this.fpsList = [];
+
+    this.gameStack = [];
+
+    this.classNameToAttribute = {
+      'gpugames_field_normal_high2': gpuAttribute.GOOD,
+      'gpugames_field_int_high': gpuAttribute.PROBABLY_GOOD,
+
+      'gpugames_field_normal_high': gpuAttribute.AVERAGE,
+      'gpugames_field_normal_unsure': gpuAttribute.PROBABLY_AVERAGE,
+      'gpugames_field_int_unsure': gpuAttribute.PROBABLY_AVERAGE,
+      'gpugames_field_int_unsure': gpuAttribute.PROBABLY_AVERAGE,
+
+      'gpugames_field_normal_low': gpuAttribute.LOW,
+      'gpugames_field_int_low': gpuAttribute.PROBABLY_LOW,
+    };
+
+    this.fetchHTMLCheerio(url)
+      .then($ => this.scrape($));
+  }
+
+  async scrape(html) {
+    await this.populateGameList(html);
+    await this.populateQualityList(html);
+    await this.populateGPUList(html);
+
+    this.notifyScrappingDone(this.gpuList);
+  }
+
+  populateGPUList(html) {
+    let gpuIndex = 0;
+
+    this.gpuList = this.scrapeIt.scrapeHTML(html, {
+      gpu: {
+        listItem: '.gpuform #sortierbare_tabelle tr.odd, .gpuform #sortierbare_tabelle tr.even',
+        data: {
+          index: { how: () => gpuIndex++ },
+          gpuChip: '.specs > a',
+          fpsList: {
+            selector: 'td:nth-child(n + 3)',
+            how: x => {
+              let fpsList = [];
+
+              x.each((index, element) => {
+                fpsList.push(this.computeFPS(index, element));
+              });
+
+              return fpsList;
+            },
+            convert: x => {
+              return x
+            }
+          }
+        }
+      }
+    });
+
+    return this.gpuList;
+  }
+
+  populateGameList(html) {
+    this.gameList = this.scrapeIt.scrapeHTML(html, {
+      gameList: {
+        listItem: '.gpuform tr.header',
+        data: {
+          singleGameList: {
+            listItem: '.gpugames_header_games',
+            selector: 'b'
+          }
+        }
+      }
+    }).gameList[0].singleGameList;
+
+    this._reloadGameStack();
+
+    return this.gameList;
+  }
+
+  populateQualityList(html) {
+    let qualityIndex = 0;
+
+    this.qualityList = this.scrapeIt.scrapeHTML(html, {
+      qualityList: {
+        listItem: '.gpuform tr:nth-child(2)',
+        data: {
+          singleQualityList: {
+            listItem: 'td.gpugames_header_settings_class > a'
+          }
+        }
+      }
+    }).qualityList[0].singleQualityList;
+
+    return this.qualityList;
+  }
+
+  computeFPS(index, element) {
+    const gameShifterClass = 'gpugames_spacer';
+    const currentSquareClass = this.cheerio(element).attr('class');
+
+    if (currentSquareClass === gameShifterClass) {
+      this.gameStack.shift();
+      return;
+    }
+
+    const attribute = this._translateClassToAttribute(currentSquareClass);
+    const game = this.gameStack[0];
+    const fps = this.cheerio(element).find('div > span > span').text();
+
+    if (this.cheerio(element).is(':last-child')) {
+      this._reloadGameStack();
+    }
+
+    return {
+      game,
+      performances: {
+        fps,
+        attribute,
+      }
+    };
+  }
+
+  _translateClassToAttribute(className) {
+    return this.classNameToAttribute[className] || gpuAttribute.UNKNOWN;
+  }
+
+  _reloadGameStack() {
+    this.gameStack = this.gameList.slice(0);
+  }
+}
+
+module.exports = NotebookCheck_FPS;
--- Scrapping/index.js
+++ Scrapping/index.js
@@ -1,7 +1,7 @@
 #!/bin/node
-
 const cli = require('cli');
 const MaterielNet_Laptop = require('./MaterielNet_Laptop');
+const NotebookCheck_FPS = require('./NotebookCheck_FPS');
 
 cli.parse({
 	product: [ 'p', 'Scrape product', true, false],
@@ -23,5 +23,6 @@ function scrapeProducts() {
 }
 
 function scrapeSources() {
-  cli.info('No sources has been scrapped.');
+  // https://www.notebookcheck.net/Computer-Games-on-Laptop-Graphics-Cards.13849.0.html?type=&sort=&archive=1&or=0&gameselect%5B%5D=464&gameselect%5B%5D=460&gameselect%5B%5D=448&gameselect%5B%5D=444&gameselect%5B%5D=434&gameselect%5B%5D=430&gameselect%5B%5D=424&gameselect%5B%5D=420&gameselect%5B%5D=414&gameselect%5B%5D=411&gameselect%5B%5D=409&gameselect%5B%5D=405&gameselect%5B%5D=407&gameselect%5B%5D=402&gameselect%5B%5D=398&gameselect%5B%5D=396&gameselect%5B%5D=386&gameselect%5B%5D=384&gameselect%5B%5D=377&gameselect%5B%5D=375&gameselect%5B%5D=371&gameselect%5B%5D=366&gameselect%5B%5D=361&gameselect%5B%5D=359&gameselect%5B%5D=332&gameselect%5B%5D=329&gameselect%5B%5D=299&gameselect%5B%5D=293&gameselect%5B%5D=265&gameselect%5B%5D=162&gameselect%5B%5D=112&gameselect%5B%5D=49&gameselect%5B%5D=53&gpu_fullname=1
+  new NotebookCheck_FPS('http://zachary.pm/mnt/fps.html');
 }
